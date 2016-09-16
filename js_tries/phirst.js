772619ddf4522ed2b71b951a83bfc62fd69539b0
Naren SivaSubramani <ufo2mstar@gmail.com>
2016-09-15 21:53:41 -0400

    JS: jquery get
    finally!

---
+++ js_tries/phirst.js
@@ -0,0 +1,4 @@
+jscript = document.createElement('script');
+jscript.src = "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
+jscript.src= "https://code.jquery.com/jquery-3.1.0.min.js"
+document.getElementsByTagName("body")[0].appendChild(jscript);

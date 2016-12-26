281f25e2d95a5608c7272e27885b3fc7b4c46ab0
Naren SivaSubramani <ufo2mstar@gmail.com>
2016-12-26 02:10:45 -0500

    CTCI: heap trouble. needs a fresh look tomorrow

--- hackerrank/ctci/hard/running_median/compare_res.rb
+++ hackerrank/ctci/hard/running_median/compare_res.rb
@@ -1,3 +1,5 @@
+p 100.times.map{|i| (i-1)/2 }
+
 puts "EXP \t - \t Res"
 
 File.open('test_res.txt', 'r') do |f|
--- hackerrank/ctci/hard/running_median/heap_median.rb
+++ hackerrank/ctci/hard/running_median/heap_median.rb
@@ -2,33 +2,61 @@
 #   File.open('test_out.txt','w'){|f| f.print(args)}
 #   puts *args
 # end
+$puts = true
 
 #!/bin/ruby
 
 class MinHeap
-  def initialize
 
+  def initialize
+    @heap = []
   end
 
-  get_parent=-> i { (i-1)/2 }
-  get_left=-> i { (i*2)+1 }
-  get_right=-> i { (i*2)+2 }
+  def list
+    @heap
+  end
 
   def add itm
-Array
+    # p @heap
+    @heap << itm
+    arrange_heap!
+    p @heap
   end
 
   private
-  def arrange_heap
+  def arrange_heap!
+    last = @heap.length - 1
+    up_check last
+  end
+
+  def up_check child
+    # get_parent=-> i { i == 0 ? i : (i-1)/2 }
+    get_parent=-> i { (i-1)/2 }
+    get_left=-> i { (i*2)+1 }
+    get_right=-> i { (i*2)+2 }
+
+    begin
+    parent = get_parent[child]
+    puts "#{child} #{parent} - #{@heap[child]} vs #{@heap[parent]}"
+    return if parent < 0
+    if @heap[child] < @heap[parent]
+      bubble_up child, parent
+      up_check parent
+    elsif child != 0 # for the first element
+      # down_check parent
+    end
+    rescue Exception => e
+      e
+    end
 
   end
 
-  def bubble_up
-
+  def bubble_up child, parent
+    @heap[child], @heap[parent] = @heap[parent], @heap[child]
   end
 
-  def bubble_down
-
+  def bubble_down child, parent
+    bubble_up parent, child
   end
 
 end
@@ -37,9 +65,11 @@ HEAP = MinHeap.new
 
 n = gets.strip.to_i
 a = Array.new(n)
+get_median=-> ary, len { len.even? ? (ary[len/2]+ary[(len-2)/2])/2.0 : ary[((len-1)/2)] }
+
 for a_i in (0..n-1)
-  a[a_i] = gets.strip.to_i
-  get_median=-> ary { len = ary.length; len.even?? (ary[len/2]+ary[(len-2)/2])/2.0 : ary[((len-1)/2)]}
-  puts get_median[a[0..a_i]].to_f
+  HEAP.add gets.strip.to_i
+  # p HEAP.list
+  puts get_median[HEAP.list, a_i+1].to_f
 end
 

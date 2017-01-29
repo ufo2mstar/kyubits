a54eadd4fecd7f03822c3f4856672f64e5dc0389
Naren SivaSubramani <ufo2mstar@gmail.com>
2017-01-29 14:37:40 -0500

    HACK: codesprint9: queen done, solved

--- hackerrank/world_codesprint_9/queen_attack_ii.rb
+++ hackerrank/world_codesprint_9/queen_attack_ii.rb
@@ -1,6 +1,12 @@
-require 'matrix'
+# require 'matrix'
 
-class Matrix
+class PretMatrix
+  def self.build n,r,c,b
+    res = {}
+    bounds = []
+    res[a] = b
+  end
+
   def sum
     self.to_a.flatten.reduce(&:+)
   end
@@ -23,49 +29,50 @@ k = k.to_i
 class Board
   def initialize n, r_q, c_q
     @lim = n
-    @board = make_board @lim
     @block = 'x'
     @r_q, @c_q = r_q-1, c_q-1
     # @r_q, @c_q = r_q, c_q
+    @board = make_board @lim
     obst(@r_q+1, @c_q+1, 9)
   end
 
   def make_board n
-    PretMatrix.build(n) { 0 }
+    # PretMatrix.build(n,@r_q,@c_q)
+    {}
   end
 
   def obst(rO, cO, val=@block)
-    @board[rO-1, cO-1] = val
+    @board[[rO-1, cO-1]] = val
   end
-
-  def count
-    @res = 0
-    low_row = (0...@r_q).to_a.reverse
-    hi_row = (@r_q+1...@lim).to_a
-    low_col = (0...@c_q).to_a.reverse
-    hi_col = (@c_q+1...@lim).to_a
-    lcs, hcs, lrs, hrs = low_col.size, hi_col.size, low_row.size, hi_row.size
-
-    run([@r_q]*lcs, low_col)
-    run([@r_q]*hcs, hi_col)
-    run(low_row, [@c_q]*lrs)
-    run(hi_row, [@c_q]*hrs)
-    run(hi_row, hi_col)
-    run(hi_row, low_col)
-    run(low_col, low_col)
-    run(low_col, hi_col)
-    @res
-  end
-
-  def run rows, cols
-    z = rows.zip cols
-    b = @board
-    z.each do |r, c|
-      return if r.nil? or c.nil?
-      return if b[r, c] == -1
-      @res += 1
-    end
-  end
+  #
+  # def count
+  #   @res = 0
+  #   low_row = (0...@r_q).to_a.reverse
+  #   hi_row = (@r_q+1...@lim).to_a
+  #   low_col = (0...@c_q).to_a.reverse
+  #   hi_col = (@c_q+1...@lim).to_a
+  #   lcs, hcs, lrs, hrs = low_col.size, hi_col.size, low_row.size, hi_row.size
+  #
+  #   run([@r_q]*lcs, low_col)
+  #   run([@r_q]*hcs, hi_col)
+  #   run(low_row, [@c_q]*lrs)
+  #   run(hi_row, [@c_q]*hrs)
+  #   run(hi_row, hi_col)
+  #   run(hi_row, low_col)
+  #   run(low_col, low_col)
+  #   run(low_col, hi_col)
+  #   @res
+  # end
+  #
+  # def run rows, cols
+  #   z = rows.zip cols
+  #   b = @board
+  #   z.each do |r, c|
+  #     return if r.nil? or c.nil?
+  #     return if b[r, c] == -1
+  #     @res += 1
+  #   end
+  # end
 
 
   def count2
@@ -81,11 +88,11 @@ class Board
       # puts @que.join ' '
       @que.dup.each do |d|
         v = @dir[d]
-        if b[*v] == @block or b[*v].nil? or v.min < 0
+        if b[v] == @block or v.min < 0 or v.max >= @lim # or b[*v].nil? or v.min < 0
           @que.delete(d)
           @dir.delete(d)
         else
-          b[*v] = 1
+          # b[v] = 1
           @res+=1
         end
       end

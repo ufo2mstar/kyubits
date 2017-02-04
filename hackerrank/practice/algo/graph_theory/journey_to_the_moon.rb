1c91806d52b1d3987ac3bde6ceea81de4b1466d6
Naren SivaSubramani <ufo2mstar@gmail.com>
2017-02-04 18:48:52 -0500

    HACK: Graph: moon start

--- hackerrank/practice/algo/graph_theory/journey_to_the_moon.rb
+++ hackerrank/practice/algo/graph_theory/journey_to_the_moon.rb
@@ -1,26 +1,62 @@
+class Node
+  attr_accessor :name, :nodes
 
-class Comb
-
-  def self.ncr(n, r)
-    a, b  = r, n-r
-    a, b  = b, a if a < b # a is the larger
-    numer = (a+1..n).inject(1) { |t, v| t*v } # n!/r!
-    denom = (2..b).inject(1) { |t, v| t*v } # (n-r)!
-    numer/denom
+  def initialize name
+    @name = name
+    @link = {}
   end
 
-  def self.npr(n, r)
-    a, b  = r, n-r
-    # a, b  = b, a if a < b # a is the larger
-    numer = (1..n).inject(1) { |t, v| t*v } # n!
-    denom = (2..b).inject(1) { |t, v| t*v } # (n-r)!
-    numer/denom
+  def add_link node
+    @link[node.name] = node
   end
-
-  def self.facto n
-    (1..n).inject(1) { |f, v| f*v } # n!
-  end
-
 end
 
+class MyTree
+  attr_accessor :name, :nodes
 
+  def initialize name
+    @name = name
+    @nodes = {}
+  end
+
+  def add_nodes node_1, node_2
+    node_1.add_link node_2
+    node_2.add_link node_1
+  end
+
+  def count
+    @nodes.keys.length # returns count of all the nodes
+  end
+end
+
+class Forest
+  attr_accessor :name, :trees , :nodes
+
+  def initialize name
+    @name = name
+    @nodes = {}
+    @trees = {}
+    @tree_count = 0
+  end
+
+  def sow node_1, node_2
+    # match, extra = [node_1,node_2] & @nodes
+    # puts "Extra #{extra}" if extra
+    match = @nodes[node_1] || @nodes[node_2]
+    tree = if match
+      @trees[match]
+    else
+      @trees[@tree_count] = MyTree.new @tree_count
+      @tree_count+=1
+    end
+    tree.add_nodes node_1, node_2
+  end
+
+  def add_tree node
+    @trees[node.name] = node
+  end
+
+  def count
+    @trees.keys.length # returns count of all the trees
+  end
+end

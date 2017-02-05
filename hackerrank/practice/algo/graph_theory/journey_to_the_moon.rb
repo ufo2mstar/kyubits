619642ff4ba3c8c26bc365b9a164f68a4cb6a6fe
Naren SivaSubramani <ufo2mstar@gmail.com>
2017-02-04 23:07:33 -0500

    HACK: Graph: moon: is it done

--- hackerrank/practice/algo/graph_theory/journey_to_the_moon.rb
+++ hackerrank/practice/algo/graph_theory/journey_to_the_moon.rb
@@ -19,7 +19,12 @@ class MyTree
     @nodes = {}
   end
 
-  def add_nodes node_1, node_2
+  def make_node node
+    @nodes[node] = Node.new node
+  end
+
+  def link_nodes node_1, node_2
+    node_1, node_2 = make_node(node_1), make_node(node_2)
     node_1.add_link node_2
     node_2.add_link node_1
   end
@@ -27,10 +32,14 @@ class MyTree
   def count
     @nodes.keys.length # returns count of all the nodes
   end
+
+  def merge tree
+    @nodes.merge tree.nodes
+  end
 end
 
 class Forest
-  attr_accessor :name, :trees , :nodes
+  attr_accessor :name, :trees, :nodes
 
   def initialize name
     @name = name
@@ -42,21 +51,95 @@ class Forest
   def sow node_1, node_2
     # match, extra = [node_1,node_2] & @nodes
     # puts "Extra #{extra}" if extra
-    match = @nodes[node_1] || @nodes[node_2]
-    tree = if match
-      @trees[match]
+    n_old, n_new = nil, nil
+    t_n1, t_n2 = @nodes[node_1], @nodes[node_2]
+    if t_n1 and t_n2
+      big_tree, small_tree = t_n1.count > t_n2.count ? [t_n1, t_n2] : [t_n2, t_n1]
+      transfer small_tree
+      big_tree.merge small_tree
+      @trees.delete(small_tree)
+    elsif t_n1.nil? and t_n2.nil?
+      add_tree node_1, node_2
+    elsif t_n1.nil?
+      tree = t_n2
+      tree_name = tree.name
+      @nodes[node_1] = tree_name
+      tree.link_nodes node_2, node_1
     else
-      @trees[@tree_count] = MyTree.new @tree_count
-      @tree_count+=1
+      tree = t_n1
+      tree_name = tree.name
+      @nodes[node_2] = tree_name
+      tree.link_nodes node_1, node_2
     end
-    tree.add_nodes node_1, node_2
   end
 
-  def add_tree node
-    @trees[node.name] = node
+  def transfer tree
+    name = tree.name
+    tree.keys.each do |node|
+      @nodes[node] = name
+    end
+  end
+
+  def add_tree node_1, node_2
+    @trees[@tree_count] = MyTree.new @tree_count
+    tree = @trees[@tree_count]
+    tree_name = tree.name
+    tree.link_nodes node_1, node_2
+    @nodes[node_1] = tree_name
+    @nodes[node_2] = tree_name
+    @tree_count+=1
   end
 
   def count
     @trees.keys.length # returns count of all the trees
   end
+
+  def compute
+    sum = 0
+    total = @nodes.length
+    @trees.each do |t|
+      r = t.count
+      n = total - r
+      sum += Comb.ncr n, r
+    end
+    sum
+  end
 end
+
+
+class Comb
+
+  def self.ncr(n, r)
+    a, b = r, n-r
+    a, b = b, a if a < b # a is the larger
+    numer = (a+1..n).inject(1) { |t, v| t*v } # n!/r!
+    denom = (2..b).inject(1) { |t, v| t*v } # (n-r)!
+    numer/denom
+  end
+
+  def self.npr(n, r)
+    a, b = r, n-r
+    # a, b  = b, a if a < b # a is the larger
+    numer = (1..n).inject(1) { |t, v| t*v } # n!
+    denom = (2..b).inject(1) { |t, v| t*v } # (n-r)!
+    numer/denom
+  end
+
+  def self.facto n
+    (1..n).inject(1) { |f, v| f*v } # n!
+  end
+
+end
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N, i = gets.split.map { |x| x.to_i }
+
+f = Forest.new 'gir'
+i.times do
+  a, b = gets.split.map { |x| x.to_i }
+  f.sow a, b
+end
+
+result = f.compute
+puts result

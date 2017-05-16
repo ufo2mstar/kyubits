a9ae51ed8789bc82479b52a1da02783c058b31da
Naren SivaSubramani <ufo2mstar@gmail.com>
2017-05-16 00:03:06 -0400

    INT: ya, you need balanced

--- other/int/int.rb
+++ other/int/int.rb
@@ -5,14 +5,14 @@ def brackets n
   return @set if n<=0
   @n = n
   recu "", n
-  puts @set.to_a
+  @set.to_a
 end
 
 def recu(str, rem)
   if rem > 0
     recu str+"(", rem-1
-    # recu str+")", rem unless is_balanced? str
-    recu str+")", rem if str.size.odd?
+    recu str+")", rem unless is_balanced? str
+    # recu str+")", rem if str.size.odd? # terrible
   else
     # while str.size < (2*@n)
     #   str+=")"
@@ -24,14 +24,14 @@ end
 
 def is_balanced? str
   stack = []
-  str.chars.each{|c|
-    c == "(" ? stack.push(c) : stack.pop
-  }
+  str.chars.each {|c| c == "(" ? stack.push(c) : stack.pop}
   stack.empty?
 end
 
-i = 6
-brackets i
+i = 5
+x = brackets i
+puts x
+puts x.size
 
 # while i == 0
 #   x = gets.chomp

a3bb834e93d49941513afbe1bfa9b6f7b781b20b
Naren SivaSubramani <ufo2mstar@gmail.com>
2017-12-09 02:41:17 -0500

    GAME: RW: 04: done

--- other/games/ruby_warrior-cli/init/rubywarrior/jon-beginner/player.rb
+++ other/games/ruby_warrior-cli/init/rubywarrior/jon-beginner/player.rb
@@ -2,7 +2,8 @@
 
 # require_relative 'lib/01' # 10
 # require_relative 'lib/02' # 36
-require_relative 'lib/03' # 103
+# require_relative 'lib/03' # 103
+require_relative 'lib/04' # 103
 
 # class Player
 #   def play_turn(warrior)

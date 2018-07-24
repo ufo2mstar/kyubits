dd8451531cc243f9eca1bc852810983140103c71
Naren SivaSubramani <ufo2mstar@gmail.com>
2018-07-23 22:39:01 -0400

    RB: RECUR: brackets recur ary pass

--- other/int/int_btr.rb
+++ other/int/int_btr.rb
@@ -1,16 +1,19 @@
 require 'set'
 
 def brackets n
-  @set = Set.new
-  build n, n, ""
-  @set
+  # @set = Set.new
+  ary = []
+  build "", n, n, ary
+  ary
+  # @set
 end
 
 
-def build str, beg, fin
-  build str+"(", beg-1, fin if beg > 0
-  build str+")", beg, fin-1 if fin > beg
-  @set << str if beg == 0 and fin == 0
+def build str, beg, fin, ary
+  build str+"(", beg-1, fin, ary if beg > 0
+  build str+")", beg, fin-1, ary if fin > beg
+  # @set << str if beg == 0 and fin == 0
+  ary << str if beg == 0 and fin == 0
 end
 
 n = 10

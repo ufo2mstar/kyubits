20e555e6225858f2d1e9fe5ba7671a80ded67181
Naren SivaSubramani <ufo2mstar@gmail.com>
2018-07-26 09:40:18 -0400

    RB: INT: REAL: two not so simple problems

--- other/int/2018_amz/real_test.rb
+++ other/int/2018_amz/real_test.rb
@@ -49,3 +49,75 @@ end
 #   return 0 if a == 0 or b == 0
 #   return (a > b ? gcd(a-b,b) : gcd(a,b-a))
 # end
+
+
+# 23280670791993
+
+
+def retrieveMostFrequentlyUsedWords(literatureText, wordsToExclude)
+  # WRITE YOUR CODE HERE
+  words = literatureText.split
+  exclude_hsh = {}
+  wordsToExclude.each{|w| exclude_hsh[w] = 1}
+
+  freq_hsh = {}
+  freq_hsh.default = 0
+
+  count_hsh = {}
+  max_count = 0
+  words.each do |word|
+    unless exclude_hsh[word]
+      freq_hsh[word]+=1
+      max_count = [max_count,freq_hsh[word]].max
+    end
+  end
+
+  freq_list = []
+  freq_hsh.each do |word, count|
+    if count == max_count
+      freq_list << word
+    end
+  end
+  freq_list
+end
+
+
+def reorderLines(logFileSize, logLines)
+  # WRITE YOUR CODE HERE
+
+  text_lines = []
+  num_lines = []
+
+  line_id = {}
+  get_line_from_id = {}
+
+  logLines.each_with_index do |line,i|
+    line_id[line] = i
+    get_line_from_id[i] = line
+
+    # separate by line type
+    line_type_check line, i, text_lines, num_lines
+
+  end
+
+  text_lines.sort!
+  # return text_lines
+  final_lines = []
+
+  text_lines.each do |line_and_id|
+    id = line_and_id.split(" ")[-1]
+    final_lines << get_line_from_id[id.to_i]
+  end
+
+  [final_lines,num_lines]
+
+end
+
+def line_type_check line, id, text_lines, num_lines
+  line_words = line.split
+  if line_words[1] =~ /\d/
+    num_lines << line
+  else
+    text_lines << (line_words[1..-1]+[line_words[0]]+[id]).join(" ")
+  end
+end

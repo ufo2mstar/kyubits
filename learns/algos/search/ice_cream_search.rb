8a93d2949f4a9ec3b1c542eeedf451ded8a8b307
Naren SivaSubramani <ufo2mstar@gmail.com>
2018-07-25 21:27:32 -0400

    RB: HACK: SEARCH: ice cream search 2

--- learns/algos/search/ice_cream_search.rb
+++ learns/algos/search/ice_cream_search.rb
@@ -1,33 +1,61 @@
-# Complete the whatFlavors function below.
+# # Complete the whatFlavors function below.
+# def whatFlavors(cost, money)
+#   cost = cost.sort
+#   sunny,johnny = 0,0
+#   cost.each_with_index do |val,s|
+#     pair = bin_search cost, val
+#     if pair != -1
+#       sunny = s+1
+#       johnny = pair+1
+#       puts "#{sunny} #{johnny}"
+#       return [sunny,johnny]
+#     end
+#   end
+#
+# end
+#
+# def bin_search ary, x
+#   return -1 if ary.empty?
+#   low = 0
+#   high = ary.size-1
+#
+#   while low<=high
+#     mid = (low+high)/2
+#     if ary[mid] == x
+#       return mid
+#     elsif ary[mid] < x
+#       low = mid+1
+#     else
+#       high = mid-1
+#     end
+#   end
+#   -1
+# end
+
 def whatFlavors(cost, money)
-  cost = cost.sort
+  cost_id = {}
+  # cost_id.default = [] # does not work
+
+  cost.each_with_index{|c,i|
+    cost_id[c] ||= []
+    cost_id[c] << i+1
+  }
+
   sunny,johnny = 0,0
   cost.each_with_index do |val,s|
-    pair = bin_search cost, val
-    if pair != -1
-      sunny = s+1
-      johnny = pair+1
+    pair = money - val
+    if cost_id[pair]
+      if cost_id[pair].size > 1
+        sunny = cost_id[pair][0]
+        johnny = cost_id[pair][1]
+      elsif cost_id[pair].size == 1
+        next if pair == val
+        sunny = cost_id[val][0]
+        johnny = cost_id[pair][0]
+      end
       puts "#{sunny} #{johnny}"
       return [sunny,johnny]
     end
   end
 
-end
-
-def bin_search ary, x
-  return -1 if ary.empty?
-  low = 0
-  high = ary.size-1
-
-  while low<=high
-    mid = (low+high)/2
-    if ary[mid] == x
-      return mid
-    elsif ary[mid] < x
-      low = mid+1
-    else
-      high = mid-1
-    end
-  end
-  -1
 end
--- learns/algos/search/ice_cream_search_spec.rb
+++ learns/algos/search/ice_cream_search_spec.rb
@@ -2,7 +2,22 @@ require 'rspec'
 require_relative 'ice_cream_search'
 
 describe :bin_search_simple do
-  context "return -1 for" do
+  context "1" do
+    it 'should empty ary' do
+      cost = [1,4,5,3,2]
+      money = 4
+      res = whatFlavors cost, money
+      expect(res).to eq [1,4]
+    end
+    it 'should empty ary' do
+      cost = [2,2,4,3]
+      money = 4
+      res = whatFlavors cost, money
+      expect(res).to eq [1,2]
+    end
+  end
+
+  context "2r" do
     it 'should empty ary' do
       cost = [1,4,5,3,2]
       money = 4

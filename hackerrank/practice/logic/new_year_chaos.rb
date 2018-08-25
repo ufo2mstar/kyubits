211d5f0af6203adda97100e3818c76ad7ea0bf2c
Naren SivaSubramani <ufo2mstar@gmail.com>
2018-08-25 22:06:35 +0530

    RB: HACK: ARY: new year chaos 2: overtake counts

--- hackerrank/practice/logic/new_year_chaos.rb
+++ hackerrank/practice/logic/new_year_chaos.rb
@@ -6,26 +6,28 @@ require 'stringio'
 # Complete the minimumBribes function below.
 def minimumBribes(q)
   bribe_count = 0
-  q.each_with_index do |person,i|
+  # q.each_with_index do |person,i|
+  i = q.size-1
+  while i >= 0 do
+    person = q[i]
     position = i+1
     offset = person - position
 
     if offset > 2
       puts "Too chaotic"
       return
-    elsif offset < 0
-      # else
-      if i < q.size-1
-        overtake = person-q[i+1]
-        bribe_count += 1 if overtake > 0
-      end
-      if i < q.size-2
-        overtake = person-q[i+2]
-        bribe_count += 2 if overtake > 0
-      end
     else
-      bribe_count += offset
+      j=i
+      while j-1 >= 0 or j+1 > person do
+        j-=1
+        if q[j] > person
+          bribe_count += 1
+        else
+          # break if j+1 == person
+        end
+      end
     end
+    i-=1
   end
   puts bribe_count
 end

5152858a8b077b2dfbc9d759017ad89f277a3267
Naren SivaSubramani <ufo2mstar@gmail.com>
2018-08-30 01:03:32 +0530

    RB: INT: AMZ: working code! good job

--- other/int/2018_amz/08-29_linked_list.rb
+++ other/int/2018_amz/08-29_linked_list.rb
@@ -4,7 +4,7 @@
 # 1->2->2->3->3->5
 
 class Node
-  attr_accrssor :val, :next_node
+  attr_accessor :val, :next_node
 
   def initialize(int)
     @val = int
@@ -27,24 +27,28 @@ ll1.add_next_node(Node.new(3))
 #
 # O(n+m)
 #
-res = nil
 
-while node1 != nil or node2 != nil
+def two_list_merge node1, node2
+  res = nil
 
-  if node1 != nil and node2 != nil
-    if node1.val < node2.val
-      res == nil ? res = Node.new(node1.val) : res.add_next_node(Node.new(node1.val))
-      node1 = node1.next_node
-    else
+  while node1 != nil or node2 != nil
+
+    if node1 != nil and node2 != nil
+      if node1.val < node2.val
+        res == nil ? res = Node.new(node1.val) : res.add_next_node(Node.new(node1.val))
+        node1 = node1.next_node
+      else
+        res == nil ? res = Node.new(node2.val) : res.add_next_node(Node.new(node2.val))
+        node2 = node2.next_node
+      end
+    elsif node1 == nil
       res == nil ? res = Node.new(node2.val) : res.add_next_node(Node.new(node2.val))
       node2 = node2.next_node
+    else
+      res == nil ? res = Node.new(node1.val) : res.add_next_node(Node.new(node1.val))
+      node1 = node1.next_node
     end
-  elsif node1 == nil
-    res == nil ? res = Node.new(node2.val) : res.add_next_node(Node.new(node2.val))
-    node2 = node2.next_node
-  else
-    res == nil ? res = Node.new(node1.val) : res.add_next_node(Node.new(node1.val))
-    node1 = node1.next_node
+
   end
 
 end
@@ -64,12 +68,19 @@ end
 # 1 -> 2 -> 3 n2
 
 def merge_lists heads
+  head = nil
   res = nil
-
   until heads.empty?
     min_val = get_min(heads)
-    res == nil ? res = Node.new(min_val) : res.add_next_node(Node.new(min_val))
+    if res == nil
+      res = Node.new(min_val)
+      head = res
+    else
+      res.add_next_node(Node.new(min_val))
+      res = res.next_node
+    end
   end
+  head
 end
 
 # O(k)
@@ -85,9 +96,9 @@ def get_min heads
 
   # min_val 1
   # heads = {<1->1->1>, <1->1->1>}
-  heads.each do |node_i|
+  heads.each_with_index do |node_i, i|
     if node_i.val == min_val
-      node_i = node_i.next_node
+      heads[i] = node_i.next_node
       heads.compact! # O(K)
       break
     end
@@ -111,9 +122,31 @@ end
 # List<Node>
 
 def make_ll(ary)
+  head = nil
   node = nil
   ary.each do |val|
-    node == nil ? (node = Node.new(val)) : node.add_next_node(Node.new(val))
+    if node == nil
+      (node = Node.new(val))
+      head = node
+    else
+      node.add_next_node(Node.new(val))
+      node = node.next_node
+    end
   end
-  node
+
+  head
+end
+
+n1 = make_ll([1, 2, 2, 4, 5, 6, 9])
+n2 = make_ll([3, 3, 8, 8, 9])
+n3 = make_ll([3, 3, 6, 10])
+n4 = make_ll([7])
+
+heads = [n1, n2, n3, n4]
+
+res = merge_lists heads
+
+while res.next_node
+  print "#{res.val} -> "
+  res = res.next_node
 end

5fae08ef7712d1dc5cbf742fcc9ce6d4eef91663
Naren SivaSubramani <ufo2mstar@gmail.com>
2018-09-22 02:22:05 -0400

    RB: INT: LEET: QTN: 94 percentile solution.. one more to go

--- leetcode/learns/hash/05_ary_intersection.rb
+++ leetcode/learns/hash/05_ary_intersection.rb
@@ -19,3 +19,24 @@
 #                What if the given array is already sorted? How would you optimize your algorithm?
 # What if nums1's size is small compared to nums2's size? Which algorithm is better?
 # What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Integer[]}
+def intersect(nums1, nums2)
+  hsh_1 = Hash.new 0
+  hsh_2 = Hash.new 0
+  intersection = []
+
+  nums1.each_with_index {|n,i|hsh_1[n] += 1}
+  nums2.each_with_index {|n,i|
+    hsh_2[n] += 1;
+    if hsh_1[n] > 0
+      intersection << n
+      hsh_1[n]-= 1;
+      hsh_2[n]-= 1;
+    end
+  }
+
+  intersection
+end

c6b65fe98016683a5d08f3b723a49d795ca415ad
Naren SivaSubramani <ufo2mstar@gmail.com>
2018-11-04 11:07:26 -0500

    RB: TREE: ITR: REFAC: splitting out order traversers

---
+++ learns/patterns/iterator/simple_itr.rb
@@ -0,0 +1,120 @@
+class Iterator
+  attr_accessor :tree
+
+  def initialize bintree, type = 'pre'
+    @tree = bintree
+    @itr = case type
+             when /pre/
+               PreorderIterator.new bintree
+             when /in/
+               InorderIterator.new bintree
+             when /post/
+               PostorderIterator.new bintree
+             else
+               raise "unknown itr type passed: #{type}"
+           end
+  end
+
+  def has_next?
+    @itr.has_next?
+  end
+
+  def next
+    @itr.next
+  end
+end
+
+
+class PreorderIterator
+  attr_accessor :tree
+
+  def initialize bintree
+    @tree = bintree
+    @stk = []
+    @stk << bintree.root if bintree.root
+  end
+
+  def has_next?
+    !@stk.empty?
+  end
+
+  def has_next_ino?
+    !@stk.empty?
+  end
+
+  def next
+    if has_next?
+      node = @stk.pop
+      @stk << node.right if node.right
+      @stk << node.left if node.left
+      node.data
+    end
+  end
+
+end
+
+
+class InorderIterator
+  attr_accessor :tree
+
+  def initialize bintree
+    @tree = bintree
+    @stk = []
+    left_seek bintree.root if bintree.root
+  end
+
+  def has_next?
+    !@stk.empty?
+  end
+
+  def next
+    if has_next?
+      node = @stk.pop
+      left_seek node.right if node.right
+      node.data
+    end
+  end
+
+  private
+  def left_seek node
+    @stk << node if node
+    while node.left do
+      node = node.left
+      @stk << node
+    end
+  end
+
+end
+
+
+class PostorderIterator
+  attr_accessor :tree
+
+  def initialize bintree
+    @tree = bintree
+    @stk = []
+    left_seek bintree.root if bintree.root
+  end
+
+  def has_next?
+    !@stk.empty?
+  end
+
+  def next
+    if has_next?
+      node = @stk.pop
+      left_seek node.right if node.right
+      node.data
+    end
+  end
+
+  private
+  def left_seek node
+    @stk << node if node
+    while node.left do
+      node = node.left
+      @stk << node
+    end
+  end
+
+end
--- learns/patterns/iterator/simple_itr_spec.rb
+++ learns/patterns/iterator/simple_itr_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require_relative 'simple_inorder_itr'
+require_relative 'simple_itr'
 
 require_relative '../../../learns/trees/bst/bin_tree_use'
 
@@ -8,7 +8,7 @@ describe Iterator do
 
     let(:ary) {[5, 3, 6, 7, 2, 4, 1, 9, 8]}
     let(:bin_tree) {BinTree.new}
-    it 'should take a bst root' do
+    it 'defaults to preorder, should take a bst root' do
       bin_tree.insert_level_order ary
       itr = Iterator.new bin_tree
       expect(itr.tree).to equal bin_tree
@@ -21,10 +21,13 @@ describe Iterator do
     let(:ary) {[6, 4, 5, 2, 3, 1, 8, 7, 10, 9, 11]}
     let(:bin_tree) {b = BinTree.new; b.insert_bst ary; b}
     let(:empty_tree) {BinTree.new}
-    let(:itr) {Iterator.new(bin_tree)}
+    let(:itr_pre) {Iterator.new(bin_tree, 'pre')}
+    let(:itr_in) {Iterator.new(bin_tree, 'in')}
+    let(:itr_post) {Iterator.new(bin_tree, 'post')}
 
     it 'has_next?' do
       empty_itr = Iterator.new empty_tree
+      itr = itr_pre
       expect(empty_itr.has_next?).to equal false
       expect(itr.has_next?).to equal true
     end
@@ -32,28 +35,31 @@ describe Iterator do
     context 'real use case' do
       it 'next pre' do
         bin_tree.show_tree
+        itr = itr_pre
         exp = [6, 4, 2, 1, 3, 5, 8, 7, 10, 9, 11]
         res = []
         while itr.has_next?
-          res << itr.next('pre')
+          res << itr.next
         end
         expect(res).to eq exp
       end
       it 'next in' do
         bin_tree.show_tree
+        itr = itr_in
         exp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
         res = []
-        while itr.has_next_ino?
-          res << itr.next('in')
+        while itr.has_next?
+          res << itr.next
         end
         expect(res).to eq exp
       end
       it 'next post' do
         bin_tree.show_tree
-        exp = [6, 4, 2, 1, 3, 5, 8, 7, 10, 9, 11]
+        itr = itr_post
+        exp = [1, 3, 2, 5, 4, 7, 9, 11, 10, 8, 6]
         res = []
         while itr.has_next?
-          res << itr.next('post')
+          res << itr.next
         end
         expect(res).to eq exp
       end

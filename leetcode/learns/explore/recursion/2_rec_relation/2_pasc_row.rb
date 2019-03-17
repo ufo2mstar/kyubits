e1dc583a63ec6d19193e40e14b3dd86f8cdbd9e9
Naren SivaSubramani <ufo2mstar@gmail.com>
2019-03-16 21:45:06 -0400

    RB: LEET: PREP: REC: pasc triangle get

---
+++ leetcode/learns/explore/recursion/2_rec_relation/2_pasc_row.rb
@@ -0,0 +1,35 @@
+# Pascal's Triangle II
+#   Go to Discuss
+# Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+#
+#     Note that the row index starts from 0.
+#
+#
+#         In Pascal's triangle, each number is the sum of the two numbers directly above it.
+#
+# Example:
+#
+# Input: 3
+# Output: [1,3,3,1]
+# Follow up:
+#
+# Could you optimize your algorithm to use only O(k) extra space?
+
+# @param {Integer} row_index
+# @return {Integer[]}
+def get_row(r)
+  @pasc = Array.new(r) {Array.new(r)}
+  ans = []
+  r.times{|i| ans[i] = rec(i,r)}
+  ans
+end
+
+def rec i,j
+  i = 0 if i < 0
+  return @pasc[i][j] if @pasc[i][j]
+  (return @pasc[i][j] = 1) if j == 1 or j == i
+  return rec(i-1,j-1) + rec(i-1,j)
+end
+
+r = 3
+get_row(r)

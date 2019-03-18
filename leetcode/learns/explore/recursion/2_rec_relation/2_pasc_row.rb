43df86c7a1d5141e74773bd219cb5dbc852bac50
Naren SivaSubramani <ufo2mstar@gmail.com>
2019-03-18 16:32:00 -0400

    RB: LEET: PREP: REC: pasc triangle row get results

--- leetcode/learns/explore/recursion/2_rec_relation/2_pasc_row.rb
+++ leetcode/learns/explore/recursion/2_rec_relation/2_pasc_row.rb
@@ -39,3 +39,51 @@ r = 15
 r = 25
 p get_row(r)
 
+# sample 44 ms submission
+
+# Given an index k, return the kth row of the Pascal's triangle.
+#
+# For example, given k = 3,
+# Return [1,3,3,1].
+#
+# Note:
+# Could you optimize your algorithm to use only O(k) extra space?
+
+# @param {Integer} row_index
+# @return {Integer[]}
+def get_row4(row_index)
+  res = [1]
+  row_index.times do
+    res = [[0] + res, res + [0]].transpose.map(&:sum)
+  end
+  res
+end
+
+p get_row(4)
+
+# @param {Integer} row_index
+# @return {Integer[]}
+def get_row3(row_index)
+  res = [1]
+  row_index.times do
+    res = [[0] + res, res + [0]].transpose.map(&:sum)
+  end
+  res
+end
+
+p get_row(4)
+
+# sample 32 ms submission
+# @param {Integer} row_index
+# @return {Integer[]}
+def get_row2(row_index)
+  [p=1]+(1..row_index).map{|i|p=p*(row_index-i+1)/i}
+end
+
+# sample 36 ms submission
+# @param {Integer} row_index
+# @return {Integer[]}
+def get_row1(row_index)
+  [p=1]+(1..row_index).map{|i|p=p*(row_index-i+1)/i}
+end
+

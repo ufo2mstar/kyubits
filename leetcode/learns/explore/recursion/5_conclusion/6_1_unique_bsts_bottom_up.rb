8496dada6adf14e193cb76a0171c83927157aff1
Naren SivaSubramani <ufo2mstar@gmail.com>
2019-04-01 11:14:11 -0400

    RB: LEET: PREP: REC: PERMU: flatten try

--- leetcode/learns/explore/recursion/5_conclusion/6_1_unique_bsts_bottom_up.rb
+++ leetcode/learns/explore/recursion/5_conclusion/6_1_unique_bsts_bottom_up.rb
@@ -11,8 +11,14 @@ end
 
 def generateTrees n
   return [] if n == 0
-  xmp = generate(1, n)
-  xmp
+  res = generate(1, n)
+  res
+end
+
+def flatten_trees res
+  ary = []
+  res.each { |root| ary << flatten_tree(root) }
+  ary
 end
 
 def generate(start, last)
@@ -33,9 +39,30 @@ def generate(start, last)
     end
   end
   res.empty? ? [nil] : res
-  # res || [nil]
 end
 
-p generateTrees 3
-p generateTrees 5
+
+def flatten_tree root
+  tree_ary = []
+  i = 0
+  flatten_tree_util root, i, tree_ary
+  tree_ary
+end
+
+def flatten_tree_util node, i, tree_ary
+  return if node.nil?
+  left_i = (2 * i) + 1
+  right_i = (2 * i) + 2
+  tree_ary[i] = node.val
+  # tree_ary[left_i] = nil if node.left
+  # tree_ary[right_i] = nil if node.right
+  flatten_tree_util node.left, left_i, tree_ary
+  flatten_tree_util node.right, right_i, tree_ary
+end
+
+require 'awesome_print'
+# p generateTrees 1
 # p generateTrees 2
+p generateTrees 3
+# p generateTrees 4
+# p generateTrees 5

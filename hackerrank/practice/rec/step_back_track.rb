0a2a0879b0201ffe5701a56867d25f1a1961ecfc
Naren SivaSubramani <ufo2mstar@gmail.com>
2019-04-10 01:30:05 -0400

    RB: HACK: PREP: REC: step mem backtrack: over count fix

--- hackerrank/practice/rec/step_back_track.rb
+++ hackerrank/practice/rec/step_back_track.rb
@@ -1,11 +1,11 @@
 # Complete the stepPerms function below.
 def stepPerms(n)
-  jumps = [1,2,3]
-  res = step(n,0,jumps,{})
-  res % (7+10e9)
+  jumps = [1, 2, 3]
+  res = step(n, jumps, {})
+  res % (7 + 10e9).to_i
 end
 
-def step n,count,jumps,mem
+def step n, jumps, mem
   if n < 0
     0
   elsif n == 0
@@ -13,12 +13,13 @@ def step n,count,jumps,mem
   elsif mem[n]
     return mem[n]
   else
+    count = 0
     jumps.each do |i|
-      count+=step n-i,count,jumps,mem
+      count += step n - i, jumps, mem
     end
     mem[n] = count
   end
 end
 
-n=5
+n = 7
 p stepPerms(n)

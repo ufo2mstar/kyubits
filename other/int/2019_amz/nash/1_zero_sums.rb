4e0eeedd54d82c2cb8382fe81bd77c6187f553e8
Naren SivaSubramani <ufo2mstar@gmail.com>
2019-04-24 17:04:34 -0400

    RB: INT: QTNS: zero sum triplets: order saving complexifying the problem

--- other/int/2019_amz/nash/1_zero_sums.rb
+++ other/int/2019_amz/nash/1_zero_sums.rb
@@ -15,17 +15,20 @@ def count_triplet_sums_n3 ary, target = 0
 end
 
 def count_triplet_sums_n2_hsh ary, target = 0
+  set = Set.new
   count = 0
-  hsh = ary.zip([]).to_h
+  hsh = ary.zip([true]*ary.size).to_h
   (0...ary.size).each do |i|
     (i + 1...ary.size).each do |j|
       x, y = ary[i], ary[j]
       z = target - (x + y)
-      if hsh[z]
-        count += 1
-        p [x, y, z]
+      if hsh[z] and z != x and z != y
+        # count += 1
+        # p [x, y, z]
+        set << [x, y, z].sort
       end
     end
   end
-  count
+  # p set.to_a
+  count = set.size
 end
--- other/int/2019_amz/nash/1_zero_sums_spec.rb
+++ other/int/2019_amz/nash/1_zero_sums_spec.rb
@@ -6,9 +6,19 @@ describe "1_zero_sums" do
     it 'sums easy vals' do
       ary = [-5, -2, 0, 2, 3]
       exp = 2
-      # res = count_triplet_sums_n3 ary
+      res = count_triplet_sums_n3 ary
+      expect(res).to eq exp
       res = count_triplet_sums_n2_hsh ary
       expect(res).to eq exp
     end
+
+    it 'sums easy vals' do
+      ary = (-5..5).to_a
+      exp = count_triplet_sums_n3 ary
+      # expect(res).to eq exp
+      res = count_triplet_sums_n2_hsh ary
+      expect(res).to eq exp
+    end
+
   end
 end

fcd59a2ef672cf7a2f854a44c7dd83d7ccfd227a
Naren SivaSubramani <ufo2mstar@gmail.com>
2019-05-03 12:58:42 -0400

    RB: TOOL: GP: lib: parser, writer init

--- tools/my_gherkin_parser/cmdline_shell/lib/gherkin_parser.rb
+++ tools/my_gherkin_parser/cmdline_shell/lib/gherkin_parser.rb
@@ -1,3 +1,25 @@
+# = Parse Gherkins into my mental models
+#
+# Init Parser and get to action on a given location
+# @
 class GherkinParser
+  attr_accessor :location
+  attr_reader :features
+
+  def initialize loc = '*'
+    @location = loc
+    @features = get_features @location
+  end
+
+  private
+
+  # if you need to ignore any pattern of files, change this regex to match those patterns
+  IGNORE_EXP = //
+
+  # dir glob for all feature files
+  # @param location dir location of all the feature files
+  def get_features(loc)
+    Dir.glob(loc).reject {|x| x =~ IGNORE_EXP}
+  end
 
 end
---
+++ tools/my_gherkin_parser/cmdline_shell/lib/report_writer.rb
@@ -0,0 +1,9 @@
+require 'writeexcel'
+
+#= Write to Reports
+#
+# to write out all your parsed gherkins by tags etc..
+# uses gem 'writeexcel'
+class ReportWriter
+
+end
